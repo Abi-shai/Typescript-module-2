28c4dc2307fee2ee2c108bcc4d72a04af2c05c19
Abishai Yapele <abishaiyp.dev@gmail.com>
2022-09-12 09:47:17 +0000

    add: more content

--- src/Company.ts
+++ src/Company.ts
@@ -1,19 +1,25 @@
 import { faker } from '@faker-js/faker'
+import { Mappable } from './CustomMap'
 
-export class Company {
-  companyName: string
+export class Company implements Mappable {
+  name: string
   catchPhrase: string
   location: {
     lat: number
     lng: number
   }
+  color: string = 'red'
 
   constructor() {
-    this.companyName = faker.company.name()
+    this.name = faker.company.name()
     this.catchPhrase = faker.company.catchPhrase()
     this.location = {
       lat: Number(faker.address.latitude()),
       lng: Number(faker.address.longitude())
     }
+  }
+
+  markerContent(): string {
+    return `Company Name: ${this.name}`
   }
 }
--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,17 +1,22 @@
+import { faker } from "@faker-js/faker"
 // Instructions to every other class
 // on how they can be an argument to 'addMarker'
-interface Mappable {
+export interface Mappable {
+  name: string
   location: {
     lat: number
     lng: number
   }
+  markerContent(): string
+  color: string
 }
 
 export class CustomMap {
   private googleMap: google.maps.Map
+  name: string
 
   constructor(reference: Element) {
-
+    this.name = faker.name.firstName()
     this.googleMap = new google.maps.Map(reference, {
       zoom: 1,
       center: {
@@ -22,12 +27,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: Number(mappable.location.lat),
         lng: Number(mappable.location.lng)
       }
+    })
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      })
+
+      infoWindow.open(this.googleMap, marker)
     })
   }
 }
